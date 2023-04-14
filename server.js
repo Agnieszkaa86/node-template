e10a91937ff3c09496f9ef09630ca77a1ab2192b
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-04-14 10:55:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 
+const path = require('path');
 
 
 const { connectDatabase } = require('./startup/database.js');
@@ -16,6 +17,8 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 app.use(logger(formatsLogger));
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
 
  require("./config/passport");
 
