ab99bbf079c75d1e0075c3ee29d1318eb0345746
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-04-14 10:55:48 +0200

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const ctrlUser = require("../../controllers/users");
+const path = require("path");
+const multer = require("multer");
+const uploadDir = path.join(process.cwd(), "tmp");
 
 const router = express.Router();
 const authorization = require("../../auth/authorization");
+
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+  limits: {
+    fileSize: 1048576,
+  },
+});
+const upload = multer({ storage });
 
 router.post("/signup", ctrlUser.signUp);
 
@@ -11,5 +27,6 @@ router.post("/login", ctrlUser.logIn);
 router.get("/logout", authorization, ctrlUser.logOut);
 
 router.get("/current", authorization, ctrlUser.current);
+router.patch("/avatars", authorization, upload.single('avatar'), ctrlUser.avatar);
 
 module.exports = router;
